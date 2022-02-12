1bf985d90a50472989ab20befed39412828d552d
jimmygarces47 <jimmygarces4@gmail.com>
2022-02-12 01:22:52 +0000

    ejercicio 6.2

--- .learn/resets/04-Print-the-last-one/app.js
+++ .learn/resets/04-Print-the-last-one/app.js
@@ -8,8 +8,3 @@ function generateRandomArray()
 var myStupidArray = generateRandomArray();
 
 //Your code here
-
- var theLastOne=myStupidArray[myStupidArray.length - 1];
- console.log(theLastOne);
- 
- 
--- .learn/resets/05.4-Add-items-to-array-looping/app.js
+++ .learn/resets/05.4-Add-items-to-array-looping/app.js
@@ -5,8 +5,3 @@ var arr = [4,5,734,43,45];
 // you need to loop 10 times, for example, using a for loop
 //*****************
 
-for (let i = 0; i< 10; i++){
-    arr.push(Math.floor(Math.random()* 5));
-    
-}
-console.log(arr);
